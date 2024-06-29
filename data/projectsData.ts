dd09d0bf1f55de2a809ec877776added99cdb6e7
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-06-29 14:03:11 +0530

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,17 @@
+interface Project {
+  title: string,
+  description: string,
+  href?: string,
+  imgSrc?: string,
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'S.H.L',
+    description: `The Home of Life, Bitches. This is what my goal is. This is what my dream is . This is what my reality is going to be. This is what God showed me as my purpose, and for that reason only I know that this dream will become a reality.`,
+    imgSrc: '/static/images/image.png',
+    href: 'https://www.google.com',
+  }
+]
+
+export default projectsData
